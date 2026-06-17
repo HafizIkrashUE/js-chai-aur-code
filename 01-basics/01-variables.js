56c2e90389dbb2cfc295b27769736a2eda8fe068
HafizIkrashUE <hafizikrash809@gmail.com>
2026-06-17 17:50:25 +0000

    variables

---
+++ 01-basics/01-variables.js
@@ -0,0 +1,7 @@
+const accountId = 21;
+let accountEmail = "hafizikrash809@gmail.com";
+let accountPassword = "2341";
+let accountCity = "lahore";
+
+console.log(accountId);
+console.table([accountId,accountEmail,accountPassword,accountCity])
